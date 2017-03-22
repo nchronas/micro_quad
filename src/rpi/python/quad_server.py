e05f95bfa269c2076d872ce7f3e9177760256fdc
nchronas <nchronas@gmail.com>
2017-03-23 01:47:52 +0200

    Trying eventlet

--- src/rpi/python/quad_server.py
+++ src/rpi/python/quad_server.py
@@ -3,17 +3,21 @@ import threading
 import os
 import socket
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+import socketio
 from time import sleep
-import json
+import eventlet
+import eventlet.wsgi
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
+async_mode = 'eventlet'
+
+sio = socketio.Server(logger=True, async_mode=async_mode)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 app.debug = True
-socketio = SocketIO(app)
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
+app.config['SECRET_KEY'] = 'secret!'
 
 tx_flag = False
 
@@ -29,18 +33,23 @@ def quad_response_parser(data):
 
 
 def tst_udp_server():
+    global tx_flag
     while True:
         if tx_flag:
             sock.sendto("$MQRPY*\n", (UDP_IP, UDP_PORT))
-        sleep(0.1)
+        #sleep(0.1)
+        eventlet.sleep(1)
 
-def udp_server():
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
     while True:
+        #sio.sleep(10)
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         dict_out = quad_response_parser(data)
+        count += 1
         print "received message:", data, dict_out
-        socketio.emit('my_response', json.dumps(dict_out), namespace='/test')
-
+        sio.emit('my_response', {'data': 'Server generated event'}, namespace='/test')
 
 @app.route('/')
 def index():
@@ -48,7 +57,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/cube')
-def index():
+def cube():
     '''View test index html.'''
     return render_template('cube.html')
 
@@ -67,23 +76,26 @@ def three():
     '''View test index html.'''
     return app.send_static_file('Three.js')
 
-
-@socketio.on('connect', namespace='/test')
-def on_connect_test():
+@sio.on('connect', namespace='/test')
+def test_connect(sid, environ):
     print "Socket Connected"
-    emit('my_response', {'data': 'Hello my friend'})
+    sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid, namespace='/test')
 
+@sio.on('disconnect', namespace='/test')
+def test_disconnect(sid):
+    print('Client disconnected')
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
+@sio.on('my_event', namespace='/test')
+def test_message(sid, message):
+    global tx_flag
     print "Received socket msg", message
-    if message.data == 'toggle_tx':
+    if message['data'] == 'toggle_tx':
         if tx_flag:
             tx_flag = False
         else:
             tx_flag = True
         print "Toggling  UDP Tx to: ", tx_flag
-    emit('my_response', {'data': 'got it!'})
+    sio.emit('my_response', {'data': message['data']}, room=sid, namespace='/test')
 
 if __name__ == '__main__':
 
@@ -97,15 +109,16 @@ if __name__ == '__main__':
 
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("0.0.0.0", UDP_PORT))
-    udp_ser = threading.Thread(target=udp_server, args=())
-    udp_ser.daemon = True
-    udp_ser.start()
 
-    udp_tst_ser = threading.Thread(target=tst_udp_server, args=())
-    udp_tst_ser.daemon = True
-    udp_tst_ser.start()
+    #udp_tst_ser = threading.Thread(target=tst_udp_server, args=())
+    #udp_tst_ser.daemon = True
+    #udp_tst_ser.start()
 
-    try:
-        socketio.run(app, host='0.0.0.0', port=8080)
-    except (KeyboardInterrupt, SystemExit):
-        socketio.stop()
+    eventlet.spawn(tst_udp_server)
+    #eventlet.spawn(background_thread)
+    #thread = sio.start_background_task(background_thread)
+
+    #try:
+    eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
+    #except (KeyboardInterrupt, SystemExit):
+        #sio.stop()
